f637e18ecd58a87fbc5966a60a8368208783b3b6
Mohamed Eid Saad <m.eid.saad403@gmail.com>
2022-03-18 18:19:22 +0200

    first commit

---
+++ database/migrations/2022_03_18_135405_create_doctor_post_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctor_post', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            $table->bigInteger('doctor_ssn')->unsigned();
+            $table->foreign('post_id')
+                  ->references('id')
+                  ->on('posts')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->foreign('doctor_ssn')
+                  ->references('ssn')
+                  ->on('doctors')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctor_posts');
+    }
+};
