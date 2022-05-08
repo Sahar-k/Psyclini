d3e281e316bb89717e2285ab2707855ec0090f21
Sahar-k <100826066+Sahar-k@users.noreply.github.com>
2022-05-08 15:51:00 +0200

    Update DoctorSeeder.php

--- database/seeders/DoctorSeeder.php
+++ database/seeders/DoctorSeeder.php
@@ -24,7 +24,11 @@ class DoctorSeeder extends Seeder
             'birth_date'=>'1985-02-04',
             'phone'=>'011111111',
             'password'=>'198524MA',
-            'email'=>'mohammadadel85@gmail.com'
+            'email'=>'mohammadadel85@gmail.com',
+            'session_fees'=>'150',
+            'experince_years'=>'4',
+            'education'=>'Master\'s degree in autism',
+            'gender'=>'male',
             ]);
          \App\Models\Doctor::create([
             'ssn'=>'8542002',
@@ -32,10 +36,14 @@ class DoctorSeeder extends Seeder
             'speciality'=>'Child and Adolescent',
             'rating'=>'4.7',
             'img' => 'people pic 7.jpg',
-            'birth_date'=>'1985-04-02',
+            'birth_date'=>'1981-04-02',
             'phone'=>'012222222',
             'password'=>'198542AA',
             'email'=>'amiraali85@gmail.com',
+            'session_fees'=>'250',
+            'experince_years'=>'7',
+            'education'=>'PhD in communication sciences',
+            'gender'=>'female',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'8265003',
@@ -47,6 +55,10 @@ class DoctorSeeder extends Seeder
             'phone'=>'013333333',
             'password'=>'198265SM',
             'email'=>'samirmohammad82@gmail.com',
+            'session_fees'=>'200',
+            'experince_years'=>'5',
+            'education'=>'Master\'s degree in geronto-psychiatry',
+            'gender'=>'male',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'8256004',
@@ -58,6 +70,10 @@ class DoctorSeeder extends Seeder
             'phone'=>'014444444',
             'password'=>'198256MA',
             'email'=>'manarahmed82@gmail.com',
+            'session_fees'=>'150',
+            'experince_years'=>'3',
+            'education'=>'Master\'s degree in geronto-psychiatry',
+            'gender'=>'female',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'8179005',
@@ -69,6 +85,10 @@ class DoctorSeeder extends Seeder
             'phone'=>'015555555',
             'password'=>'198179AA',
             'email'=>'ahmedabbas81@gmail.com',
+            'session_fees'=>'200',
+            'experince_years'=>'7',
+            'education'=>'Master\'s degree in mental disorders',
+            'gender'=>'male',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'8197006',
@@ -80,6 +100,10 @@ class DoctorSeeder extends Seeder
             'phone'=>'016666666',
             'password'=>'198197SA',
             'email'=>'salahabdalah81@gmail.com',
+            'session_fees'=>'100',
+            'experince_years'=>'4',
+            'education'=>'PhD in mental disorders',
+            'gender'=>'male',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'8354007',
@@ -91,7 +115,12 @@ class DoctorSeeder extends Seeder
             'phone'=>'017777777',
             'password'=>'198354WM',
             'email'=>'walaamohammad83@gmail.com',
+            'session_fees'=>'200',
+            'experince_years'=>'6',
+            'education'=>'Master\'s degree in marital relations',
+            'gender'=>'female',
             ]);
+
         \App\Models\Doctor::create([
             'ssn'=>'8345008',
             'name'=>'Amir Karim',
@@ -102,6 +131,10 @@ class DoctorSeeder extends Seeder
             'phone'=>'018888888',
             'password'=>'19845AK',
             'email'=>'amirkarim83@gmail.com',
+            'session_fees'=>'100',
+            'experince_years'=>'4',
+            'education'=>'PHD in marital relations',
+            'gender'=>'male',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'8775009',
@@ -113,6 +146,10 @@ class DoctorSeeder extends Seeder
             'phone'=>'019999999',
             'password'=>'198775MK',
             'email'=>'marinakamil87@gmail.com',
+            'session_fees'=>'100',
+            'experince_years'=>'4',
+            'education'=>'Bachelor of Psychology',
+            'gender'=>'female',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'87570010',
@@ -124,6 +161,10 @@ class DoctorSeeder extends Seeder
             'phone'=>'099999999',
             'password'=>'198757SS',
             'email'=>'samarsaid87@gmail.com'
+            'session_fees'=>'200',
+            'experince_years'=>'4',
+            'education'=>'Master\'s degree in Psychology',
+            'gender'=>'female',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'75890011',
@@ -135,17 +176,25 @@ class DoctorSeeder extends Seeder
             'phone'=>'011333333',
             'password'=>'197589NA',
             'email'=>'naderali75@gmail.com',
+            'session_fees'=>'250',
+            'experince_years'=>'10',
+            'education'=>'PhD in addiction',
+            'gender'=>'male',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'75980012',
             'name'=>'Kamal Mohammad',
             'speciality'=>'Addiction treatment',
-            'rating'=>'4.3',
+            'rating'=>'4.7',
             'img' => 'people pic 8.jpg',
-            'birth_date'=>'1975-09-08',
+            'birth_date'=>'1979-09-08',
             'phone'=>'011444444',
             'password'=>'197598KM',
             'email'=>'kamalmohammad75@gmail.com',
+            'session_fees'=>'250',
+            'experince_years'=>'9',
+            'education'=>'PhD in Psychology',
+            'gender'=>'male',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'88811013',
@@ -153,10 +202,14 @@ class DoctorSeeder extends Seeder
             'speciality'=>'life coach',
             'rating'=>'4.7',
             'img' => 'people pic 8.jpg',
-            'birth_date'=>'1198-08-18',
+            'birth_date'=>'1988-08-18',
             'phone'=>'011555555',
             'password'=>'1988811AA',
             'email'=>'ahmedabdehghane88@gmail.com'
+            'session_fees'=>'150',
+            'experince_years'=>'4',
+            'education'=>'Bachelor of Psychology',
+            'gender'=>'male',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'88118014',
@@ -164,10 +217,14 @@ class DoctorSeeder extends Seeder
             'speciality'=>'life coach',
             'rating'=>'4.4',
             'img' => 'people pic 8.jpg',
-            'birth_date'=>'1198-01-028',
+            'birth_date'=>'1981-01-28',
             'phone'=>'011666666',
             'password'=>'1988118AA',
             'email'=>'aminali88@gmail.com'
+            'session_fees'=>'200',
+            'experince_years'=>'6',
+            'education'=>'Bachelor of Psychology',
+            'gender'=>'male',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'91189015',
@@ -175,10 +232,14 @@ class DoctorSeeder extends Seeder
             'speciality'=>'Biological psychiatry',
             'rating'=>'3.9',
             'img' => 'people pic 8.jpg',
-            'birth_date'=>'1199-01-01',
+            'birth_date'=>'1991-01-01',
             'phone'=>'011777777',
             'password'=>'1991189SA',
             'email'=>'saidali91@gmail.com'
+            'session_fees'=>'100',
+            'experince_years'=>'4',
+            'education'=>'Master\'s degree in Obsessive-compulsive disorder',
+            'gender'=>'male',
             ]);
         \App\Models\Doctor::create([
             'ssn'=>'84189016',
@@ -186,10 +247,14 @@ class DoctorSeeder extends Seeder
             'speciality'=>'Biological psychiatry',
             'rating'=>'4.9',
             'img' => 'people pic 8.jpg',
-            'birth_date'=>'1198-01-04',
+            'birth_date'=>'1988-01-04',
             'phone'=>'011888888',
             'password'=>'1984189MS',
             'email'=>'moustafasaid84@gmail.com',
+            'session_fees'=>'150',
+            'experince_years'=>'6',
+            'education'=>'Master\'s degree in schizophrenia',
+            'gender'=>'male',
             ]);
     }
 }
